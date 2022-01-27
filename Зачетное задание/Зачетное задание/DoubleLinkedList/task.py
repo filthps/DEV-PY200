465957dde3e7f0de510bd6ca5a6713fa12d231f7
Литовченко Виктор Иванович <VIKLIT@avalon.ru>
2022-01-27 21:37:29 +0300

    Подготовка к зачёту.

--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -17,7 +17,7 @@ class LinkedList(MutableSequence):
     def append(self, value) -> None:
         new_node = self.NODE_TYPE(value)
         if self._head is None:
-            self._head = new_node
+            self._head = new_node  # fixme tail??? over test case
         else:
             last_node = self.move_from_head(self._length)
             self.add_link(last_node, new_node)
@@ -26,15 +26,15 @@ class LinkedList(MutableSequence):
     def insert(self, index: int, value) -> None:
         node = self.NODE_TYPE(value)
         if index == 0:
-            next_elem = self._head
-            self.add_link(node, next_elem)
+            next_elem = self._head  # fixme лишнее
+            self.add_link(node, next_elem)  # self.add_link(node, self._head)
             self._head = node
-        elif index == self._length - 1:
+        elif index == self._length - 1:  # fixme test case insert(does_not_exist_index) index > len
             last_elem = self.move_from_head(self._length - 1)
             self.add_link(last_elem, node)
         else:
             elem = self.move_from_head(index - 1)
-            self.add_link(elem, node)
+            self.add_link(elem, node) # fixme test case in middle
         self._length += 1
 
     @staticmethod
@@ -42,11 +42,12 @@ class LinkedList(MutableSequence):
         left_node.next = right_node
 
     @classmethod
-    def is_valid(cls, node):
+    def is_valid(cls, node):  # todo зачем???
+        """Зачем"""
         if not isinstance(node, cls):
             raise TypeError
 
-    def move_from_head(self, index):
+    def move_from_head(self, index):  # todo negative index, index error???
         current_node = self._head
         for _ in range(index):
             next_node = current_node.next
@@ -55,9 +56,11 @@ class LinkedList(MutableSequence):
                 return current_node
 
     def __getitem__(self, i):
+        # fixme TypeError IndexError
         return self.move_from_head(i)
 
     def __setitem__(self, i, value):
+        # fixme TypeError IndexError
         self.insert(i, value)
 
     def __delitem__(self, i):
@@ -70,7 +73,7 @@ class LinkedList(MutableSequence):
         return str(self.to_list())
 
     def to_list(self):
-        return [item for item in self]
+        return [item for item in self]  # todo через магический метод
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.to_list()})"
@@ -79,13 +82,13 @@ class LinkedList(MutableSequence):
         current_node = self._head
         while True:
             yield current_node
-            current_node = current_node.next
+            current_node = current_node.next  # test case ???
 
     def __iter__(self):
         return self.iterator()
 
     def __contains__(self, item):
-        for node in self.to_list():
+        for node in self.to_list(): # in self
             if node == item:
                 return True
         return False
@@ -93,9 +96,7 @@ class LinkedList(MutableSequence):
 
 class DoubleLinkedList(LinkedList):
     NODE_TYPE = DoubleLinkedNode
-    
-    def __init__(self, items):
-        super().__init__(items)
+
 
     @staticmethod
     def add_link(left_node: Node, right_node: Node):
